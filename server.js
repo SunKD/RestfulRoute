d30ec49974e72cd90eb5776025fc5187799d066c
Sun Davey <sundavey@hotmail.com>
2018-05-09 16:52:28 -0700

    Angular framework added

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const xp = require('express');
 const bp = require('body-parser');
+const path = require('path');
 
 const app = xp();
 app.use(bp.json());
+
+app.use(xp.static(path.join(__dirname , './HelloAngular/dist/HelloAngular' )));
 
 app.listen(8000, function(){
     console.log("Server is running!");
